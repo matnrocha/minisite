8119d66fcb8f745ad08696ed5aee7c99b9dd6a81
Gonçalo Simões <151958998+goncaloosimoes@users.noreply.github.com>
2025-10-28 15:03:40 +0000

    github pages

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -1,35 +1,29 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Taça UA',
+  title: 'Taça UA Minisite',
   tagline: 'A maior competição inter-cursos de Portugal',
   favicon: 'img/favicon.ico',
 
-  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
-    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+    v4: true,
   },
 
-  // Set the production url of your site here
-  url: 'https://tacaua.pt',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // Configuração para GitHub Pages
+  url: 'https://taca-ua.github.io',
+  baseUrl: '/minisite/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Taca-Ua', // Usually your GitHub org/user name.
-  projectName: 'taca-ua', // Usually your repo name.
+  organizationName: 'Taca-Ua',
+  projectName: 'minisite',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'pt',
     locales: ['pt'],
@@ -87,7 +81,7 @@ const config: Config = {
           position: 'left',
           label: 'Documentação',
         },
-        {to: '/blog', label: 'Notícias', position: 'left'},
+        { to: '/blog', label: 'Notícias', position: 'left' },
         {
           href: 'https://github.com/Taca-Ua',
           label: 'GitHub',
